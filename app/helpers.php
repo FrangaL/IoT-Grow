2f7f513cb4633327baf068b310341c61ab8033dc
Son <frangal@gmail.com>
2020-08-24 20:20:38 +0200

    Fix rtc_get_date

--- app/helpers.php
+++ app/helpers.php
@@ -18,19 +18,9 @@ function btnact($gpio)
     }
 }
 
-// function rtc_get_date()
-// {
-//     $date = shell_exec("sudo ds1302_get_utc");
-//     $rtc_time = substr($date, 0, 4) . "-" . substr($date, 5, 2) . "-" . substr($date, 8, 2) . " " . substr($date, 11, 2) . ":" . substr($date, 14, 2) . ":" . substr($date, 17, 2);
-//     return $rtc_time;
-// }
-
 function rtc_get_date()
 {
-	$pid_rtc = pid_open("/dev/rtc0");
-	$date = pid_ioctl($pid_rtc, "get date");
-	pid_close($pid_rtc);
-
-	$rtc_time = substr($date, 0, 4) . "-" . substr($date, 4, 2) . "-" . substr($date, 6, 2) . " " . substr($date, 8, 2) . ":" . substr($date, 10, 2) . ":" . substr($date, 12, 2);
-	return $rtc_time;
+        $date = shell_exec("date +'%Y-%m-%d %H:%M:%S'");
+        $rtc_time = substr($date, 0, 4) . "-" . substr($date, 5, 2) . "-" . substr($date, 8, 2) . " " . substr($date, 11, 2) . ":" . substr($date, 14, 2) . ":" . substr($date, 17, 2);
+        return $rtc_time;
 }
